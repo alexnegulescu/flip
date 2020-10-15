e7a3b17ad1877bfc0edb324cdf0eb314a220ef83
alexnegulescu <71865036+alexnegulescu@users.noreply.github.com>
2020-10-15 10:42:07 -0400

    Create flip.cpp
    my main file at the moment

---
+++ flip.cpp
@@ -0,0 +1,20 @@
+/*
+Alex Negulescu      negulescu.a@northeastern.edu
+Ethan Chen			chen.eth@northeastern.edu
+This file is the main function of the flip game. The file includes the "deck.h"
+header file, creates an object of the deck class, and then prints it out.
+Finally, this file also calls the shuffle function from the deck header file
+and then also prints out that shuffled deck.
+*/
+
+#include "deck.h"
+
+using namespace std;
+
+//main function
+int main()
+{
+	deck d;
+
+	cout << d;
+}
